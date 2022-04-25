f3a79dccbb3f422f9e30a8796d77cc2005f6e24e
Pavel <89213158645p@gmail.com>
2022-04-25 10:25:04 +0300

    seminar 3 completed

---
+++ Seminar3/Program.cs
@@ -0,0 +1,53 @@
+﻿int[] CreateRandomArray(int size)
+{
+    int[] array = new int [size];
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-9,10);
+    }
+
+    return array;
+} 
+// Задаем сами массив
+/*
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write("Input " + i + "element: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return array;
+} 
+*/
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+void FindSunOfElements(int[] array) // 1 -4 3 -2 6 -1 5 3 8 -5
+{
+    int plusSum = 0;
+    int minusSum = 0;
+
+    for(int i = 0; i <array.Length; i++)
+    {
+        if(array[i] > 0)
+            plusSum += array[i];
+        else
+            minusSum += array[i];
+    }
+    Console.WriteLine("Sum of positive elements is " + plusSum);
+    Console.WriteLine("Sum of negative elements is " + minusSum);
+}
+
+
+int[] myArray = CreateRandomArray(12);
+ShowArray(myArray);
+FindSunOfElements(myArray);
